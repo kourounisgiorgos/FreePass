463e237663b68b6b7a74c3f10a8e6536fae121d2
Losraio <it21985@hua.gr>
2022-05-29 12:08:25 +0300

    Added mailserver

--- src/main/java/gr/hua/freepass/CitizenController.java
+++ src/main/java/gr/hua/freepass/CitizenController.java
@@ -1,11 +1,12 @@
 package gr.hua.freepass;
 
 import gr.hua.freepass.models.Citizen;
+import gr.hua.freepass.models.EmailController;
 import gr.hua.freepass.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import javax.mail.MessagingException;
 import java.util.Optional;
 
 @RestController
@@ -28,21 +29,23 @@ public class CitizenController {
     }
 
     @PutMapping(value="/oaed/updateCitizen/{user_name}")
-    public void updateCitizen(@RequestBody String status, @PathVariable String user_name){
+    public void updateCitizen(@RequestBody String status, @PathVariable String user_name) throws MessagingException {
         Citizen updateCitizen =citizenRepo.findByUserName(user_name);
         updateCitizen.setStatus(status);
         citizenRepo.save(updateCitizen);
-
         appRepo.delete(appRepo.findByUserName(user_name));
+        EmailController.sendEmail(updateCitizen.getEmail(), "Application update", "Your application status has been updated. Please log in :)");
     }
 
     @PutMapping(value="/oasa/enableCitizenPass/{user_name}")
-    public void enableCitizenPass(@RequestBody String durationAndCode, @PathVariable String user_name){
+    public void enableCitizenPass(@RequestBody String durationAndCode, @PathVariable String user_name) throws MessagingException {
         String[] parts = durationAndCode.split("-");
 
         Citizen updateCitizen =citizenRepo.findByUserName(user_name);
         updateCitizen.setDuration(parts[1]);
         updateCitizen.setPassCode(parts[0]);
         citizenRepo.save(updateCitizen);
+        EmailController.sendEmail(updateCitizen.getEmail(), "Password enabled", "Your free pass has been enabled.!!1111");
+
     }
 }
---
+++ src/main/java/gr/hua/freepass/models/EmailController.java
@@ -0,0 +1,31 @@
+package gr.hua.freepass.models;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+
+
+
+public final class EmailController {
+
+    public static void sendEmail(String to, String subject, String text) throws MessagingException {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("127.0.0.1");
+        mailSender.setProtocol("smtp");
+        mailSender.setPort(1025);
+        mailSender.setPassword("");
+        mailSender.setUsername("");
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setFrom("applications@freepass.gr");
+        mimeMessageHelper.setText(text);
+        mimeMessageHelper.setTo(InternetAddress.parse(to));
+        mimeMessageHelper.setSubject(subject);
+
+        mailSender.send(mimeMessage);
+
+    }
+
+}
